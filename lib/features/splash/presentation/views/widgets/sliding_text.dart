32012a2513579fddaf3276abb0c0bd65f4167bca
Mostafa Atef <mostafaatef908070@gmail.com>
2023-10-05 00:11:54 +0300

    Navigat to HomeView using Getx

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({super.key, required this.slidingAnimation});
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (ctx, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text("Open your mind",
+            textAlign: TextAlign.center ,style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),),
+          );
+        });
+  }
+}
