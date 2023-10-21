00e90fa5a930622862318955063e1ca76c6cea1d
Mostafa Atef <mostafaatef908070@gmail.com>
2023-10-21 16:49:28 +0300

    create cubit method for books photos

--- lib/features/home/presentation/manager/books_photos_cubit/books_photos_cubit.dart
+++ lib/features/home/presentation/manager/books_photos_cubit/books_photos_cubit.dart
@@ -1,10 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repo/home.repo.dart';
+import 'package:dartz/dartz.dart';
 
 import 'package:equatable/equatable.dart';
 
 part 'books_photos_state.dart';
 
 class BooksPhotosCubit extends Cubit<BooksPhotosState> {
-  BooksPhotosCubit() : super(BooksPhotosInitial());
+  BooksPhotosCubit(this.homeRepo) : super(BooksPhotosInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchBooksPhotos() async {
+    emit(BooksPhotosLoading());
+      var result = await homeRepo.fetchBooksPhotos();
+      result.fold((failure) => emit(BooksPhotosFailure(errMsg: failure.errMsg)),
+          (books) => emit(BooksPhotosSuccess(books)));
+
+  }
 }
